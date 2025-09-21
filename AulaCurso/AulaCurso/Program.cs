7ab0df6a3df84e5006df03d340baff92c12344cf
Matheus-Sleutjes <sleutjes13@gmail.com>
2025-09-21 13:57:27 -0400

    primeiro commit

---
+++ AulaCurso/AulaCurso/Program.cs
@@ -0,0 +1,79 @@
+﻿//Basico em C#
+
+// Tipagem de variaveis
+var idade = 18; // inteiro
+var dataDeHoje = DateTime.Now; // data e hora
+var nomeCompleto = "Matheus Silva"; // string
+var valorDePi = 3.14; // double
+var estaChovendo = false; // booleano
+var letra = 'A'; // char 
+var numeroGrande = 1234567890123456789L; // long
+var numeroDecimal = 19.99M; // decimal
+var numeroFlutuante = 3.14F; // float
+string[] nomes = ["Joao", "Vinicius"]; // array de strings
+var nomes2 = new List<string> {"Joao", "Vinicius"}; // lista de strings
+
+// Operadores Logicos
+// ==, !=, >, <, >=, <=, &&, ||, !
+// Igualdade ==
+// Diferente !=
+// Maior que >
+// Menor que <
+// Maior ou igual >=
+// Menor ou igual <=
+// E lógico && 
+// Ou lógico ||
+// Negação lógica !
+
+
+// Operadores Aritmeticos
+// +, -, *, /, %, ++, --
+// Adição +
+// Subtração -
+// Multiplicação *
+// Divisão /
+// Módulo %
+// Incremento ++
+// Decremento --
+
+
+// Estruturas de controle
+if (idade >= 18)
+{
+    Console.WriteLine("Maior de idade");
+}
+else
+{
+    Console.WriteLine("Menor de idade");
+}
+
+switch (idade)
+{
+    case 18:
+        Console.WriteLine("Tem 18 anos");
+        break;
+    case 19:
+        Console.WriteLine("Tem 19 anos");
+        break;
+    default:
+        Console.WriteLine("Idade desconhecida");
+        break;
+}
+
+// Laços de repetição
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine(nomes[i]);
+}
+
+foreach (var nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+while (idade < 21)
+{
+    Console.WriteLine("Ainda não é adulto");
+    idade++;
+}
